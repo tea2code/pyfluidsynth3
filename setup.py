366ee4196ee807eef3efaf5caea360d554287acf
tea2code <stefan4875@gmail.com>
2013-05-23 18:37:19 +0200

    Added setup file. Closes #13.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyfluidsynth3',
+    version = '1',
+    description = "Fluidsynth bindings for Python 3.",
+    author = 'Stefan Gfroerer',
+    url = 'https://github.com/tea2code/pyfluidsynth3',
+    packages = ['pyfluidsynth3'],
+)
